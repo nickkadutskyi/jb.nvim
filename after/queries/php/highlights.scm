f63dcdc96bf6adb8215b72930175f3858a06d73c
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-14 08:25:03 -0800

    Adds phpdoc styles

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -13,3 +13,7 @@
   "?>"
 ] @tag
 
+; Adds builtin functions to the function scope
+(function_call_expression
+  function: (name) @function.builtin
+  (#match? @function.builtin "^(isset|empty|unset|array|list|echo|print|die|exit|eval|include|include_once|require|require_once)$"))
