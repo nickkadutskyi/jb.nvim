348f75a75016ad1d7eb801f9002dec7cbfdf6167
caiubi <caiubi.fonseca@syngenta.com>
2025-01-24 22:17:21 +0000

    feat: adds ruby support

---
+++ after/queries/ruby/highlights.scm
@@ -0,0 +1,6 @@
+;; extends
+
+((identifier) @function.builtin
+  (#any-of? @function.builtin "delegate" "it" "context" "describe" "before" "after" "include_context" "post" "get" "put" "delete")
+  (#set! "priority" 126))
+
