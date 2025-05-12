b9defb735d946f7a062925149258851ca2750359
Nick Kadutskyi <nick@kadutskyi.com>
2025-05-12 14:29:43 -0700

    feat(blade): adds highlightsand injectsions

--- after/queries/blade/highlights.scm
+++ after/queries/blade/highlights.scm
@@ -11,3 +11,13 @@
 (parameter) @none
 (text) @string
 
+; From https://medium.com/@jogarcia/laravel-blade-on-neovim-ee530ff5d20d
+(directive) @function
+(directive_start) @function
+(directive_end) @function
+(comment) @comment
+((parameter) @include (#set! "priority" 110))
+((php_only) @include (#set! "priority" 110))
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
+(keyword) @function
