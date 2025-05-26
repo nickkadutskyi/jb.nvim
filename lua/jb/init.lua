9a2a81f4cb4e874c57fc9cdedeb8fad929bd97ae
Nick Kadutskyi <nick@kadutskyi.com>
2025-05-25 22:27:22 -0700

    feat(trouble): generates icons for trouble.nvim

--- lua/jb/init.lua
+++ lua/jb/init.lua
@@ -234,6 +234,8 @@ function M.load(opts)
         vim.api.nvim_set_hl(0, "NavicIcons" .. icon, { link = "JBIcon" .. icon })
         -- nvim-cmp icons
         vim.api.nvim_set_hl(0, "CmpItemKind" .. icon, { link = "JBIcon" .. icon })
+        -- trouble icons
+        vim.api.nvim_set_hl(0, "TroubleIcon" .. icon, { link = "JBIcon" .. icon })
     end
 end
 
