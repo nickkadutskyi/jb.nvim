6429db3e0b430841c53c850d3b84a1a13be22213
Nick Kadutskyi <nick@kadutskyi.com>
2025-07-01 10:52:14 -0700

    markdown: make it more specific

---
+++ after/queries/markdown/highlights.scm
@@ -0,0 +1,10 @@
+;; extends
+
+(setext_h1_underline) @markup.heading.marker
+(setext_h2_underline) @markup.heading.marker
+(atx_h1_marker) @markup.heading.marker
+(atx_h2_marker) @markup.heading.marker
+(atx_h3_marker) @markup.heading.marker
+(atx_h4_marker) @markup.heading.marker
+(atx_h5_marker) @markup.heading.marker
+(atx_h6_marker) @markup.heading.marker
--- samples/sample2.jsx
+++ samples/sample2.jsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
       >
         <someTextWithEntityRefs>&amp; &#x00B7;</someTextWithEntityRefs>
         <div className="w-1/5 mx-auto my-0" > {children} </div>
+        <h1>Vite + React</h1>
       </body>
     </html>
   );
