26260b2ea8cb88e1bee10d6b83896647b2f57397
Nick Kadutskyi <nick@kadutskyi.com>
2025-12-15 21:27:12 -0800

    code injections for php to highlight in strings

--- after/queries/php_only/injections.scm
+++ after/queries/php_only/injections.scm
@@ -4,3 +4,28 @@
 ((string_content) @injection.content
   (#match? @injection.content "^[/#~].*[/#~][imsxADSUXu]*$")
   (#set! injection.language "regex"))
+
+; /* */-style Comments
+((comment) @injection.language
+  . ; this is to make sure only adjacent comments are accounted for the injections
+  [
+    (string
+      (string_content) @injection.content)
+    (encapsed_string
+      (string_content) @injection.content)
+  ]
+  (#gsub! @injection.language "/%*%s*([%w%p]+)%s*%*/" "%1")
+  (#set! injection.combined))
+
+; //-style Comments
+((comment) @injection.language
+  . ; this is to make sure only adjacent comments are accounted for the injections
+  [
+    (string
+      (string_content) @injection.content)
+    (encapsed_string
+      (string_content) @injection.content)
+  ]
+  (#gsub! @injection.language "//%s*([%w%p]+)%s*" "%1")
+  (#set! injection.combined))
+
