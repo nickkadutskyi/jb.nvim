582f1b584734606f4e363d80ae6a50f41bb90d96
Nick Kadutskyi <nick@kadutskyi.com>
2025-12-15 21:38:20 -0800

    php to support phpstorm like injections with // language=

--- after/queries/php_only/injections.scm
+++ after/queries/php_only/injections.scm
@@ -29,3 +29,14 @@
   (#gsub! @injection.language "//%s*([%w%p]+)%s*" "%1")
   (#set! injection.combined))
 
+; // language=-style Comments like in PhpStorm
+((comment) @injection.language
+  . ; this is to make sure only adjacent comments are accounted for the injections
+  [
+    (string
+      (string_content) @injection.content)
+    (encapsed_string
+      (string_content) @injection.content)
+  ]
+  (#gsub! @injection.language "//%s*language=%s*([%w%p]+)%s*" "%1")
+  (#set! injection.combined))
