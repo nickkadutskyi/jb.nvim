899a835e5e550edb97e7b01aa4b44c122ad14d94
Nick Kadutskyi <nick@kadutskyi.com>
2026-08-07 05:01:21 -0700

    feat: conventional commit style for different footer format

--- after/queries/jjdescription/highlights.scm
+++ after/queries/jjdescription/highlights.scm
@@ -6,8 +6,16 @@
   (#offset-lua-match! @punctuation.special.jjdescription "^BREAKING CHANGE:"))
 
 ((body_line) @punctuation.special.jjdescription
+  (#lua-match? @punctuation.special.jjdescription "^BREAKING CHANGE #")
+  (#offset-lua-match! @punctuation.special.jjdescription "^BREAKING CHANGE #"))
+
+((body_line) @punctuation.special.jjdescription
   (#lua-match? @punctuation.special.jjdescription "^BREAKING%-CHANGE:")
   (#offset-lua-match! @punctuation.special.jjdescription "^BREAKING%-CHANGE:"))
+
+((body_line) @punctuation.special.jjdescription
+  (#lua-match? @punctuation.special.jjdescription "^BREAKING%-CHANGE #")
+  (#offset-lua-match! @punctuation.special.jjdescription "^BREAKING%-CHANGE #"))
 
 ; Other footers: token + ": " or " #" (git trailer style)
 ((body_line) @attribute
@@ -17,4 +25,5 @@
 
 ((body_line) @attribute
   (#lua-match? @attribute "^%a[%w%-]* #")
+  (#not-lua-match? @attribute "^BREAKING%-CHANGE #")
   (#offset-lua-match! @attribute "^%a[%w%-]*"))
